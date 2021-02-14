bfa101150a91c8add554b35c94773080c1c862e7
Abid Hossain <hosainabid@gmail.com>
2021-02-14 17:01:24 +0600

    Added Image Title

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ const showImages = (images) => {
   images.forEach(image => {
     let div = document.createElement('div');
     div.className = 'col-lg-3 col-md-4 col-xs-6 img-item mb-2';
-    div.innerHTML = ` <img class="img-fluid img-thumbnail" onclick=selectItem(event,"${image.webformatURL}") src="${image.webformatURL}" alt="${image.tags}">`;
+    div.innerHTML = ` <img class="img-fluid img-thumbnail" onclick=selectItem(event,"${image.webformatURL}") src="${image.webformatURL}" alt="${image.tags}"> <h5 class="text-center">Title: ${image.tags}</h5>`;
     gallery.appendChild(div)
   })
   toggleSpinner();
